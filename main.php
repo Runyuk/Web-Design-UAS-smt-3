517b3732e6baa9c3939db1cbed5307fb72de0780
Runyuk <mafalrygodmode@gmail,com>
2024-12-26 00:18:12 +0700

    first commit

---
+++ main.php
@@ -0,0 +1,15 @@
+<select name="hari"> 
+    <option 
+    value="senin">Senin</option> 
+    <option 
+    value="selasa">Selasa</option> 
+    <option value="rabu">Rabu</option> 
+    <option 
+    value="kamin">Kamis</option> 
+    <option 
+    value="jumat">Jumat</option> 
+    <option 
+    value="sabtu">Sabtu</option> 
+    <option 
+    wvalue="minggu">Minggu</option> 
+</select> 
